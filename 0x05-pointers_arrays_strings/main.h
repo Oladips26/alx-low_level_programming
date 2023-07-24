a181f5ba6fcaa044af28d88121d43ab5541b13de
root <Oladipupooladdimeji@gmail.com>
2023-07-24 14:02:11 -0700

    cr7

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,14 +1,22 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * reset_to_98 - a function that takes a pointer to an int a
-s parameter
- * and updates the value it points to 98
- * @n: input
- * Return: n
- */
+#include <stdio.h>
+#include <unistd.h>
 
-void reset_to_98(int *n)
-{
-	*n = 98;
-}
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+int _putchar(char c);
+
+
+#endif
