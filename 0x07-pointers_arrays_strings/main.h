dd071fe79419f077b8db3d54709f2cd3285823a6
root <Oladipupooladdimeji@gmail.com>
2023-08-01 11:55:25 -0700

    felix

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,39 @@
+
+E325: ATTENTION
+Found a swap file by the name ".main.h.swp"
+          owned by: root   dated: Thu Jul 20 09:26:52 2023
+         file name: /main.h
+          modified: no
+         user name: root   host name: 261612e964a9
+        process ID: 697
+While opening file "main.h"
+      CANNOT BE FOUND
+(1) Another program may be editing the same file.  If this is the case,
+    be careful not to end up with two different instances of the same
+    file when making changes.  Quit, or continue with caution.
+(2) An edit session for this file crashed.
+    If this is the case, use ":recover" or "vim -r main.h"
+    to recover the changes (see ":help recovery").
+    If you did this already, delete the swap file ".main.h.swp"
+    to avoid this message.
+
+Swap file ".main.h.swp" already exists!
+[O]pen Read-Only, (E)dit anyway, (R)ecover, (D)elete it, (Q)uit, (A)bortFound a swap file by the name ".main.h.swp"
+          owned by: root   dated: Thu Jul 20 09:26:52 2023
+         file name: /main.h
+          modified: no
+         user name: root   host name: 261612e964a9
+        process ID: 697
+While opening file "main.h"
+      CANNOT BE FOUND
+(1) Another program may be editing the same file.  If this is the case,
+    be careful not to end up with two different instances of the same
+    file when making changes.  Quit, or continue with caution.
+(2) An edit session for this file crashed.
+    If this is the case, use ":recover" or "vim -r main.h"
+    to recover the changes (see ":help recovery").
+    If you did this already, delete the swap file ".main.h.swp"
+    to avoid this message.
+
+Swap file ".main.h.swp" already exists!
+[O]pen Read-Only, (E)dit anyway, (R)ecover, (D)elete it, (Q)uit, (A)bort:  
